cd240c3633316fe2acaae49c27c95cd12e55cc27
Olle Wiklund <owl@live.se>
2017-06-17 13:31:37 +0200

    Removed directory created by accident

--- manifests/install.pp
+++ manifests/install.pp
@@ -20,19 +20,21 @@ class setup::install inherits setup{
   }
 
   package { 'build-essential':
-    ensure => latest,
   }
 
   package { 'cmake':
-    ensure => latest,
   }
 
   package { 'python-dev':
-    ensure => latest,
   }
 
   package { 'python3-dev':
-    ensure => latest,
+  }
+
+  package {'clang':
+  }
+
+  package {'python-pip':
   }
 
 }
